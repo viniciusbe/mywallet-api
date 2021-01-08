f4442e09e35687f716bf729b32e210d1e139f512
viniciusbe <vinicius.bernardesds@gmail.com>
2021-01-08 18:05:31 -0300

    Backend Challenge

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,35 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  ManyToOne,
+  UpdateDateColumn,
+} from 'typeorm';
+
+import Category from './Category';
+
+@Entity('transactions')
 class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @Column()
   type: 'income' | 'outcome';
 
+  @Column()
   value: number;
 
-  category_id: string;
+  @ManyToOne(() => Category)
+  category_id: Category;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
